eae5369a9e6d7d8bb0403a8f0351ff96b3be9a38
mgigauri5-ux <m_gigauri5@cu.edu.ge>
2025-11-05 20:59:40 +0400

    Add files via upload

---
+++ დავალება 1.py
@@ -0,0 +1,58 @@
+print("Hello world")
+
+n1 = 2
+n2 = 4
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 // n2)
+print(n1 % n2)
+print(n1 ** n2)
+
+
+d1 = float(input("დიაგონალი1"))
+d2 = float(input("დიაგონალი2"))
+area = (d1 * d2) / 2
+print("რომბის ფართობია:", area )
+
+
+n1 = float(input("მეტრი"))
+print(n1 * 100)
+print(n1 * 10)
+print(n1 * 1000)
+print(n1 / 1609.34)
+
+b = float(input("ფუძე"))
+h = float(input("სიმაღლე"))
+s = (b * h) / 2
+print ("სამკუთხედის ფართობია:", s )
+
+
+number = int(input("შეიყვანეთ ორნიშნა რიცხვი: "))
+tens = number // 10
+ones = number % 10
+digit_sum = tens + ones
+
+print("ციფრთა ჯამია:", digit_sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
